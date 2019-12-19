2d1879d038089d43002f40001556ea0491a1162c
Petar Korponaić <petar.korponaic@gmail.com>
2019-12-19 17:53:31 +0100

    Release Candidate 2

--- publish.sh
+++ publish.sh
@@ -4,3 +4,5 @@ rm -rf ./dist/
 python3 setup.py sdist bdist_wheel
 twine check dist/*
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+#twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+twine upload dist/*
