82418e017603b8cb6fbf7406c09d2ef5944be36f
Mirko Rajkovaca <mirko@mrcode.dev>
2020-01-10 14:02:04 +0100

    - if input dictionary for qobj->pyquil conversion is empty return empty list instead throwing exception

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,6 +1,19 @@
+# This code is part of quantastica.qconvert
+#
+# (C) Copyright Quantastica 2019.
+# https://quantastica.com/
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 import unittest
-import warnings
 from quantastica.qconvert import convert, Format
+
 
 class TestConvert(unittest.TestCase):
     def setUp(self):
@@ -9,24 +22,19 @@ class TestConvert(unittest.TestCase):
     def tearDown(self):
         pass
 
-
     def test_invalid_params(self):
         self.assertRaises(
-            RuntimeError
-            , convert, Format.PYQUIL, dict(), Format.TOASTER
-            )
+            RuntimeError, convert, Format.PYQUIL, dict(), Format.TOASTER
+        )
 
         self.assertRaises(
-            RuntimeError
-            , convert, Format.PYQUIL, dict(), Format.PYQUIL
-            )
-
+            RuntimeError, convert, Format.PYQUIL, dict(), Format.PYQUIL
+        )
 
     def test_valid_params_empty_dict(self):
         ret = convert(Format.QOBJ, dict(), Format.TOASTER)
-        self.assertEqual( None, ret )
-        
+        self.assertEqual(len(ret), 0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
